8dca39a8e30aaac9b5bb5f106939bb7e307a836a
Jeremy-Gold <22jgold@gannacademy.org>
2020-06-24 15:00:18 -0400

    Learned H TML and CSS

--- day3/index.js
+++ day3/index.js
@@ -3,6 +3,7 @@ const path = require("path")
 const app = express()
 
 app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/assets"))
 const views = path.join(__dirname, "views")
 
 app.get("/", (req, res) => {
@@ -11,6 +12,19 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile("about.html", { root: views })
 })
+app.get("/horse", (req, res) => {
+  res.sendFile("horse.html", { root: views })
+})
+app.get("/tiger", (req, res) => {
+  res.sendFile("tiger.html", { root: views })
+})
+app.get("/me", (req, res) => {
+  res.sendFile("me.html", { root: views })
+})
+app.get("/lion", (req, res) => {
+  res.sendFile("lion.html", { root: views })
+})
+
 
 app.listen(3000)
 console.log("Running at port 3000")
